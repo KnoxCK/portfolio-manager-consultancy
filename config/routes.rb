a27f0c643ce4dea829f6e122e5fc1f76fd96ba59
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-08-11 17:41:54 +0100

    download brochure done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
-   get 'whatwedo', to: 'pages#whatwedo'
+  get 'whatwedo', to: 'pages#whatwedo'
+  get 'downloadbrochure', to: 'application#download_brochure'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
