f807541dd22a43e2ae979c2125921d983e335ccf
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-08-11 17:27:13 +0100

    dynamic news html

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+News.create(title: 'News 1', date: '08-08-2017', body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit facilis aperiam harum animi unde! Tempora rem, deleniti reiciendis repudiandae placeat, commodi quaerat blanditiis error incidunt quis maxime?")
+News.create(title: 'News 2', date: '07-08-2017', body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit facilis aperiam harum animi unde! Tempora rem, deleniti reiciendis repudiandae placeat, commodi quaerat blanditiis error incidunt quis maxime?")
+News.create(title: 'News 3', date: '06-08-2017', body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit facilis aperiam harum animi unde! Tempora rem, deleniti reiciendis repudiandae placeat, commodi quaerat blanditiis error incidunt quis maxime?")
+News.create(title: 'News 4', date: '05-08-2017', body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit facilis aperiam harum animi unde! Tempora rem, deleniti reiciendis repudiandae placeat, commodi quaerat blanditiis error incidunt quis maxime?")
+News.create(title: 'News 5', date: '04-08-2017', body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit facilis aperiam harum animi unde! Tempora rem, deleniti reiciendis repudiandae placeat, commodi quaerat blanditiis error incidunt quis maxime?")
+News.create(title: 'News 6', date: '03-08-2017', body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit facilis aperiam harum animi unde! Tempora rem, deleniti reiciendis repudiandae placeat, commodi quaerat blanditiis error incidunt quis maxime?")
