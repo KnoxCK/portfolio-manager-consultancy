6c8af645771ada5a3ee055338b77a75f76dc7d0b
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-08-11 16:12:09 +0100

    admin console added and working sexy

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,4 +1,5 @@
 RailsAdmin.config do |config|
+
   config.authorize_with do |controller|
     redirect_to main_app.root_path unless current_user && current_user.admin
   end
@@ -39,5 +40,6 @@ RailsAdmin.config do |config|
     ## With an audit adapter, you can add:
     # history_index
     # history_show
+
   end
 end
