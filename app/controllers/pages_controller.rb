cf1c0265b38711a96f0023f11ca49adc18219e67
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-08-23 13:37:55 +0100

    html and css for what we do home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :whatwedo]
 
   def home
+    @what_we_do = WhatWeDo.all
     @team_members = Team.all
     @news = News.all
   end
