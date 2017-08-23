6865d31ff00426c4eeeff3e12a0da602783d2aed
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-08-23 15:48:31 +0100

    download brochure fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: [:download_brochure]
 
   def download_brochure
     send_file "#{Rails.root}/app/assets/PMCL_LandingPage.pdf", type: "application/pdf", x_sendfile: true
