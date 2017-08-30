c5d59cd4c3d1fa11247e21b8c2330b5f5d18de21
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-08-30 22:57:56 +0100

    linked in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830213851) do
+ActiveRecord::Schema.define(version: 20170830214312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(version: 20170830213851) do
     t.string   "tel"
     t.string   "email"
     t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "linked_in_url"
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,7 +48,6 @@ ActiveRecord::Schema.define(version: 20170830213851) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
-    t.string   "linked_in_url"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
