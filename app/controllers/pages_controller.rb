14ade967a6dd2b8de9bbde81f2f62bc55b140eb1
Govinder Marwaha <govinder.marwaha@gmail.com>
2017-09-10 16:46:09 +0100

    news order reversed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,12 @@ class PagesController < ApplicationController
   def home
     @what_we_do = WhatWeDo.all
     @team_members = Team.all
-    @news = News.all
+    @news = News.all.reverse
   end
 
   def whatwedo
     @what_we_do = WhatWeDo.all
-    @news = News.all
+    @news = News.all.reverse
   end
 
   def terms
